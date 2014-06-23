5e4dcf83b0906c8727ec8673de08ee3578054671
Joel Nieto <joelnietotec@yahoo.com>
2014-06-23 17:36:43 -0500

    Estilo Valida

--- app/models/fase.rb
+++ app/models/fase.rb
@@ -1,2 +1,4 @@
 class Fase < ActiveRecord::Base
+  validates :codigo, uniqueness: {message: "Este codigo de Fase ya existe"}
+  validates :nombre, uniqueness: {message: "Este nombre de Fase ya existe"}
 end
