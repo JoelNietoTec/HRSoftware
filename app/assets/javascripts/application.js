0a599261cabb6a10bfa38b2c19ab33c1a8635ccf
Joel Nieto <joelnietotec@yahoo.com>
2014-07-03 14:46:33 -0500

    cambiar columnas de MD a SM

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,17 +51,3 @@ $(function() {
   });
 });
 
-jQuery(document).ready(function() {
-
-  jQuery('#empleado_departamento_id').change(function() {
-
-    var data=$('#empleado_departamento_id').val();
-
-    $.ajax({
-      type: "POST",
-      url: "http://"+location.host+"cargos_dinamicos/"+data,
-      data: data,
-
-    });
-  });
-});
