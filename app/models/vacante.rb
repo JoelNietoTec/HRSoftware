052df703530eed2beb0a12b49e9e8db45e3b4638
Joel Nieto <joelnietotec@yahoo.com>
2014-07-20 23:21:17 -0500

    Prospectos Pestañas

--- app/models/vacante.rb
+++ app/models/vacante.rb
@@ -3,6 +3,10 @@ class Vacante < ActiveRecord::Base
   self.per_page = 6
   #  Filtros
   def self.search(search, page)
-    order('created_at DESC').where(area_profesional_id: search).paginate(page: page, per_page: 6)
+    unless search.blank?
+      order('created_at DESC').where(area_profesional_id: search).paginate(page: page, per_page: 6)
+    else
+      order('created_at DESC').paginate(page: page, per_page: 6)
+    end
   end
 end
