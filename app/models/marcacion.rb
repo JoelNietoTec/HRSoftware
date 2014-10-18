cafc7c6a33dd37b08d18af0312b9bcab0b4032b9
Joel Nieto <joelnietotec@yahoo.com>
2014-10-17 22:28:18 -0500

    Intento Carga 1

--- app/models/marcacion.rb
+++ app/models/marcacion.rb
@@ -1,12 +1,13 @@
 class Marcacion < ActiveRecord::Base
 
   def self.import(file)
+     marcacions_attributes = [:id, :departament, :name, :no, :date, :location, :id_number, :verify_code, :card_no]
     spreadsheet = open_spreadsheet(file)
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
       marcacion = find_by_id(row["id"]) || new
-      marcacion.attributes = row.to_hash.slice(:department, :name, :no, :date, :location, :id_number, :verify_code, :card_no)
+      marcacion.attributes = row.to_hash.slice(*marcacions_attributes)
       marcacion.save!
     end
   end
@@ -21,6 +22,6 @@ class Marcacion < ActiveRecord::Base
   end
 
   def accessible_attributes
-    [department, name, no, date, location, id_number, verify_code, card_no]
+    [:id, :departament, :name, :no, :date, :location, :id_number, :verify_code, :card_no]
   end
 end
